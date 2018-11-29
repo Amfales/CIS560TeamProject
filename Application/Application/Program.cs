423476a2a0a7986a348af6f5aec06656c7ca9754
Andrew Fales <andrewfales@hotmail.com>
2018-11-28 23:15:25 -0600

    random stuff

--- Application/Application/Program.cs
+++ Application/Application/Program.cs
@@ -17,55 +17,28 @@ namespace ServerApplication
     {
         public static void Main(string[] args)
         {
-            
+
             IPAddress localhost = new IPAddress(new byte[] { 127, 0, 0, 1 });
-            WebSocketServer serv = new WebSocketServer(9999);
+            //WebSocketServer serv = new WebSocketServer(9999);
             WebServer server = new WebServer(12345);
-            serv.AddWebSocketService<TestBehavior>("/test", () => (new TestBehavior(Console.WriteLine)));
-            WebSocket ws = new WebSocket("ws://localhost:9999/test");
+            //serv.AddWebSocketService<TestBehavior>("/test", () => (new TestBehavior(Console.WriteLine)));
+            WebSocket ws = new WebSocket("ws://localhost:12345/library");
             //WebSocket ws = new WebSocket("ws://localhost:9999/", onMessage: DoPrint, onError: DoError);
-            serv.Start();
-            /*
-            LoginRequest lr = new LoginRequest("username", "password");
-            IMessage m = lr;
-            Login l = m.Payload as Login;
-            LoginRequest t = new LoginRequest(Message<Login>.UpgradeMessage(m));
-            if (l != null)
-            {
-                Console.WriteLine("UserName: " + l.UserName);
-            }
-            else
-            {
-                Console.WriteLine("l is null");
-            }*/
-            
+            //serv.Start();
 
+            server.Start();
             ws.OnMessage += (sender, e) => { Console.WriteLine(e.Data); };
+            ws.Connect();
+            ws.Send(Newtonsoft.Json.JsonConvert.SerializeObject(
+                new LoginRequest("nehelgeson@ksu.edu", "31415926712345")
+                ));
 
-            DoThings(ws);
-            string msg = "";
-            while ((msg = Console.ReadLine()) != "")
-            {
-                ws.Send(msg);
-            }
-            ws.Send("end");
-            serv.Stop();
+
             while (true)
             {
                 Console.ReadLine();
             }
             
-        }
-        
-
-        public static void DoThings(WebSocket w)
-        {
-            //w = new WebSocket(address, onMessage: DoPrint, onError: DoError);
-            
-            w.Connect();
-            //w.
-            Console.WriteLine("Did connect.");
-            w.Send("This is a message");
         }
     }
 }
