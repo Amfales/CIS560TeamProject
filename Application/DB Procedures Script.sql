86db2daa13dd8540c5206973dbf8ed95a91ae51b
Grant Willford <gwillford@ksu.edu>
2018-11-30 12:32:11 -0600

    push

--- Application/DB Procedures Script.sql
+++ Application/DB Procedures Script.sql
@@ -669,12 +669,14 @@ AS
 	DECLARE @UserID INT;
 	EXEC Proj.GetIDFromEmail @Email, @UserID OUTPUT;
 	
-	SELECT CO.BookID,BI.Title, A.FirstName AS AuthorFirstName, A.LastName AS AuthorLastName, BI.ISBN, BI.CopyrightYear, P.Name AS PublisherName, CO.CheckOutDate,CO.DueDate
+	SELECT CO.BookID,BI.Title, A.FirstName AS AuthorFirstName, A.LastName AS AuthorLastName, BI.ISBN, BI.CopyrightYear, P.Name AS PublisherName,G.Descriptor AS Genre, CO.CheckOutDate,CO.DueDate
 	FROM Book.CheckOut CO
 		INNER JOIN Book.Book B ON CO.BookID=B.BookID
 		INNER JOIN Book.BookInfo BI ON B.BookInfoID=BI.BookInfoID
 		INNER JOIN Book.Author A ON A.AuthorID=BI.AuthorID
 		INNER JOIN Book.Publisher P ON P.PublisherID=BI.PublisherID
+		INNER JOIN Book.BookGenre BG ON BG.BookInfoID=BI.BookInfoID
+		INNER JOIN Book.Genre G ON G.GenreID=BG.GenreID
 	WHERE CO.UserID=@UserID AND CO.ReturnDate IS NULL
 	ORDER BY CO.CheckOutDate ASC
 GO
